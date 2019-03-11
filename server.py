a8c32961e5e2e88f5edb978d0a02c4fae3410a17
Emma Dayton <emma.dayton85@gmail.com>
2019-03-11 13:47:02 -0700

    playing around with trying to cause errors in mysql queries

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ def index():
     mysql = connectToMySQL('first_flask')	        # call the function, passing in the name of our db
     friends = mysql.query_db('SELECT * FROM friends;')  # call the query_db function, pass in the query as a string
     print(friends)
+    newfriend = mysql.query_db("SELECT first_name FROM friends;")
+    print(friends)
     return render_template("index.html", friends=friends)
 
 if __name__ == "__main__":
